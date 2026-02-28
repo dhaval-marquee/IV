ff455cc88cd7341619e871b8b94c481fdd75ef04
Keyur <dhaval.marquee@gmail.com>
2026-02-28 12:00:45 +0530

    Cefaly - MapC - Stor-it

--- 2026/Map_communications/Reduced Hero Banner - Mobile Only/27-02/javascript.js
+++ 2026/Map_communications/Reduced Hero Banner - Mobile Only/27-02/javascript.js
@@ -1,7 +1,19 @@
+// Time difference calculation
+let Time1 = new Date().getTime()/1000;
+console.log('Time 1 > ' + Time1);
+
 function loadTestCode() {
     if (!document.querySelector('body').classList.contains('ivHeroBanner')) {
         document.querySelector('body').classList.add('ivHeroBanner');
         document.querySelectorAll('body main .home-banner picture > source')[0].setAttribute('srcset','https://www.mapcommunications.com/wp-content/uploads/2025/07/home-banner.webp');
+
+        let Time2 = new Date().getTime()/1000;
+console.log('Time 2 > ' + Time2);
+
+let timeDifference = Time2 - Time1;
+console.log('Time difference > ' + timeDifference + ' seconds');
+// Time difference calculation over
+
     }
 }
 
